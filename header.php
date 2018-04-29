49f45e0735643166a8b462ccee887bfaa1c78e5d
Ambah <ambahmuller88@gmailcom>
2018-04-29 16:40:09 +0200

    Header and Fpoter
    PHP header an footer pages to call in all php pages for html input

---
+++ header.php
@@ -0,0 +1,23 @@
+<?php
+  session_start();
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Unplanned Pregnancy</title>
+	<!--Bootstrap CDN (Content Delivery Network)-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<!--Link for JQuery and JavaScript stuff-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="style.css" rel="stylesheet">
+
+</head>
+<body>
